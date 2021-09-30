4a22cd820d451e9473cae1ef0188589084041230
Edvarts Krakts <edvarts.krakts@gmail.com>
2021-09-30 16:35:39 +0300

    Izpildīju četrus uzdevumus

--- Exercises/Level1/Exercises.Level1/Array1.cs
+++ Exercises/Level1/Exercises.Level1/Array1.cs
@@ -203,6 +203,8 @@ namespace Exercises.Level1
         }
 
 
+
+
         /// <summary>
         /// Given an array of ints, return the sum of the first 2 elements in the array. If the array
         /// length is less than 2, just sum up the elements that exist, returning 0 if the array is length
